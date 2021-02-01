e066155e2942bc38d6194e1bdfbffd6dfe8686ed
Etienne Papegnies <recon@vlay.org>
2021-02-01 14:32:07 +0100

    Added guidelines for a new exercise

--- main.php
+++ main.php
@@ -21,5 +21,14 @@ foreach ($points as $p) {
     echo $p."\n";
 }
 
+/*
+Exercice: écrivez un programme qui génère:
+-> 1 point A dont x est dans [0, 10] et y dans [0, 10] (random)
+-> 1 point B dont x est dans [20, 30] et y dans [20, 30] (random)
+-> écrivez une fonction qui calcule l'aire du rectangle dont
+   le coin suppérieur gauche est A et dont le coin inférieur
+   droit est B.
+*/
+
 
 ?>
